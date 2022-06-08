f8ddd26296f14b20cc0f848965b26a5a76c8fef6
kjbrooks1 <kjbrooks1@mac.com>
2022-06-08 09:24:24 -0400

    Initial Commit

---
+++ Boids/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Boids
+//
+//  Created by Katherine Brooks on 6/8/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
